41a9ab34977a6adef872d02a6023e3ea0b7b1d6a
imdeni <im.deniachmad@gmail.com>
2023-12-25 19:15:51 +0700

    practice interface

---
+++ practice/OOP/practiceAbstaction/interface/Program.cs
@@ -0,0 +1,9 @@
+﻿using GameControllerLib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player(1, "dee");
+        Console.WriteLine(player.Id + " " + player.Name);
+    }
+}
