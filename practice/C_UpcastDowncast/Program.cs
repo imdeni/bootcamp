c0a75ef55c0ef437f94ae80ebaf4b16060f0aa97
imdeni <im.deniachmad@gmail.com>
2023-12-25 19:42:43 +0700

    value type, refference type, upcast, downcast, child parent child

---
+++ practice/C_UpcastDowncast/Program.cs
@@ -0,0 +1,9 @@
+﻿static void Main() {
+	int a  = 3;
+	double b = a;
+	b.Dump();
+	
+	double c = 3.1;
+	int d = (int)c;
+	d.Dump();
+}
---
+++ practice/D_ChildParentChild/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program {
+	static void Main() {		
+		Child c = new Child();
+		Parent p = c;
+		Child child = (Child)p;
+		child.MethodParent();
+		child.MethodChild();
+	}
+}
+public class Parent {
+	public void MethodParent() {
+		"MethodParent".Dump();
+	}
+}
+public class Child : Parent
+{
+	public void MethodChild()
+	{
+		"MethodChild".Dump();
+	}
+}
+public static class Extensions
+{
+	public static void Dump(this object x) 
+	{
+		Console.WriteLine(x.ToString());
+	}
+}
